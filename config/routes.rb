1e9cf49ed462f08387d55692c8f75ec5c93c988d
Koen van Erp <vanerpkoen@gmail.com>
2018-11-01 17:18:36 +0100

    Some works, most doesnt

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # =beginget 'doses/create', to: 'doses#create', as: :create
+  # get 'doses/destroy', to: 'doses#destroy', as: :destroy
+  # get 'doses/new', to: 'doses#new', as: :new
+  # get 'cocktails/index', to: 'cocktails#index', as: :index
+  # get 'cocktails/show', to: 'cocktails#show', as: :show
+  # get 'cocktails/edit', to: 'cocktails#edit', as: :edit
+  # get 'cocktails/create', to:'cocktails#create', as: :create
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html=end
+   root "cocktails#index"
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
