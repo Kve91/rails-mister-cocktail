3ce6ef104121cf07a28edfa05a76bf48e07874b9
Koen van Erp <vanerpkoen@gmail.com>
2018-11-02 15:35:10 +0100

    improved the styling

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,8 @@ class Cocktail < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
-
 end
+class Cocktail < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
+end
+
